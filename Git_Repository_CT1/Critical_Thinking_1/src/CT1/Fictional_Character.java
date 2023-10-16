5468a79761d88fab675761119b6aba87cff99a99
Spy011 <148019813+Spy011@users.noreply.github.com>
2023-10-15 21:40:48 -0400

    Add files via upload

---
+++ Git_Repository_CT1/Critical_Thinking_1/src/CT1/Fictional_Character.java
@@ -0,0 +1,15 @@
+package CT1;
+
+public class Fictional_Character {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Spongebob");
+		System.out.println("Squarepants");
+		System.out.println("124 Conch St.");
+		System.out.println("Bikini Bottom");
+		System.out.println("96970");
+	}
+
+}
---
+++ Git_Repository_CT1/Critical_Thinking_1/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Critical_Thinking_1 {
+}
